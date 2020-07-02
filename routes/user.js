adb6e86254b1c60cfa4b7d2c5cea4c625de386be
Maxim <mcgrelka@yandex.ru>
2020-07-02 17:45:32 +0300

    fix routes

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const { profile } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { profileValidation } = require('../middlewares/joiUserValidation');
 
-routes.get('/user/me', profileValidation, auth, profile);
+routes.get('/users/me', profileValidation, auth, profile);
 
 module.exports = routes;
