e53c3137a540e2cca6b0cffad31fb5cbac0356ac
Maxim <mcgrelka@yandex.ru>
2020-07-30 15:52:42 +0300

    1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(mongoUri, {
 });
 
 const corsOptions = {
-  origin: '*',
+  origin: ['https://news.prnmxm.xyz/', 'http://localhost:8080/', 'https://prnmxm.xyz/', 'https://prnmxm.github.io/news-js/', 'https://prnmxm.github.io/', 'https://prnmxm.github.io/'],
   methods:['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
